ce28711950b8148b0ed0d86558feafda32142fbe
timkinsman <tkinsm@gmail.com>
2022-10-16 13:54:42 +1030

    copy fixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,14 +13,14 @@ export const works = [
     link: 'https://eatclub.com.au/',
     role: 'Frontend Developer',
     when: 'Dec 2021 - Present',
-    what: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+    what: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
   },
   {
     where: 'Allscripts',
     link: 'https://www.allscripts.com/',
     role: 'Software Engineer',
     when: 'Nov 2020 - Dec 2021',
-    what: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    what: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
   },
--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -107,9 +107,9 @@ export const Main = () => {
             <Space direction='vertical'>
               <Typography variant='h3'>Hi there!</Typography>
               <Typography variant='h5' style={{ whiteSpace: 'pre-line' }}>
-                {`I'm Tim Kinsman, a web developer based in Adelaide, Australia. Things happen to me.
+                {`I'm Tim Kinsman, a React web developer at EatClub based in Adelaide, Australia.
                 
-                "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.`}
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.`}
               </Typography>
               <Space>
                 <Typography variant='body1'>
@@ -148,8 +148,10 @@ export const Main = () => {
           >
             <Space direction='vertical'>
               <Typography variant='h1'>music</Typography>
-              <Typography variant='h5'>
-                I love music, i play guitar and bass, wow, i know, unheard of
+              <Typography variant='h5' style={{ whiteSpace: 'pre-line' }}>
+                {`Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.`}
               </Typography>
               {listeningTo.status === ApiStatusCode.succeeded && (
                 <Space style={{ alignItems: 'center' }} gap='small'>
